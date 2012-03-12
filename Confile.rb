90b28075a4e017d8ed1ced769ce8e102dc478a5d
7rans <transfire@gmail.com>
2012-03-12 16:06:31 -0400

    Look at possible use of confile for detroit. [admin]

---
+++ Confile.rb
@@ -0,0 +1,57 @@
+#!/usr/bin/env ruby
+
+#
+# QED test coverage report using SimpleCov.
+#
+# coverage_folder - the directory in which to store coverage report
+#                   this defaults to `log/coverage`.
+#
+config :qed, :profile=>:cov do
+  require 'simplecov'
+
+  dir = properties.coverage_folder
+
+  SimpleCov.start do
+    coverage_dir(dir || 'log/coverage')
+    #add_group "Label", "lib/qed/directory"
+  end
+end
+
+#
+# Example configuration.
+#
+config :example do
+  puts "Configuration Example!"
+end
+
+#
+# Detroit assembly.
+#
+config :detroit do
+  email do
+    mailto 'ruby-talk@ruby-lang.org', 'rubyworks-mailinglist@googlegroups.com'
+  end
+
+  gem do
+    active true
+  end
+
+  github do
+    folder 'web'
+  end
+
+  dnote do
+    title  'Source Notes'
+    output 'log/notes.html'
+  end
+
+  locat do
+    output 'log/locat.html'
+  end
+
+  vclog do
+    output 'log/history.html',
+           'log/changes.html'
+  end
+end
+
